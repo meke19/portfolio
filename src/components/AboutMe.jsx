33b3413767f0aa3eb2be8e2c61a84161df9639b7
meke19 <mlkmarcos13@gmail.com>
2023-04-14 15:53:32 -0300

    Responsividade

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,4 +1,4 @@
-import ImgAbout from '../img/ImgAbout.jpeg'
+import ImgAbout from '../img/ImgAbout2.jpeg'
 import "../styles/components/aboutMe.sass"
 
 export default function AboutMe(){
@@ -9,7 +9,7 @@ export default function AboutMe(){
                     <h2>Olá meu nome é <span>Marcos</span></h2>
                     <p>Sou formado em técnico de informática e atualmente estou cursando Análise e desenvolvimento
                         de sistemas, estou em busca de uma oportunidade de trabalho. Tecnologia é algo que sempre
-                        me atraiu, com isso o estudo dessa área e tudo que engloba esse mundo, 
+                        me atraiu, o estudo dessa área e tudo que engloba esse mundo, 
                         mesmo em excesso é sempre prazeroso.
                     </p>
                 </div>
--- src/components/Navegation.jsx
+++ src/components/Navegation.jsx
@@ -2,10 +2,19 @@ import "../styles/components/Navegation.sass"
 import logo from '../img/logo.png'
 
 const Navegation = () => {
+    const toggleHamburger = () => {
+        let linksArea = document.querySelector(".linksArea")
+        linksArea.classList.toggle("active")
+    }
     return(
         <header className="header">
             <div className="logoArea">
                 <img src={logo} alt='logomarca'></img>
+            </div>
+            <div className="hamburger" onClick={toggleHamburger}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
             </div>
             <div className="linksArea">
                 <ul>
