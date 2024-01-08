9ca28f053ea825d2d39f68589b01bc6273d839dc
meke19 <mlkmarcos13@gmail.com>
2024-01-08 18:17:42 -0300

    Ajustando os projetos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,9 +18,10 @@ export default function Projects() {
       </div>
       <div className="card-project">
         <div className="card">
-         <a href="https://movie-star-iota.vercel.app/" className="movie" target="_blank"></a>
-         <a href="https://clima-steel-alpha.vercel.app/" className="clima" target="_blank"></a>
-         <a href="https://pokeweb-eta.vercel.app/" className="poke" target="_blank"></a>
+          <a href="https://movie-star-iota.vercel.app/" className="movie" target="_blank"></a>
+          <a href="https://clima-steel-alpha.vercel.app/" className="clima" target="_blank"></a>
+          <a href="https://pokeweb-eta.vercel.app/" className="poke" target="_blank"></a>
+          <a href="https://luna-baloes.vercel.app/" className="luna" target="_blank"></a>
           <div className="pro">
             <div className="breve">
               <p>EM BREVE</p>
@@ -31,12 +32,6 @@ export default function Projects() {
               <p>EM BREVE</p>
             </div>
           </div>
-          <div className="pro">
-            <div className="breve">
-              <p>EM BREVE</p>
-            </div>
-          </div>
-
         </div>
       </div>
     </section>
